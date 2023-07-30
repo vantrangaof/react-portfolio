b64947bd13086c5dde62ae1021f0667fede20432
trangvan <trang@akocommerce.com>
2023-07-30 16:04:46 +0700

    arrange layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter } from 'react-router-dom'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+
 const App = () => {
 
   return (
-   <div>
-     Trang`s 3D Porfolio
+   <BrowserRouter>
+   <div className="z-0 relative bg-primary">
+    <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <Navbar />
+      <Hero />
+    </div>
+    <About />
+    <Experience />
+    <Tech />
+    <Works />
+    <Feedbacks />
+    <div className="relative z-0">
+      <Contact />
+      <StarsCanvas />
+    </div>
    </div>
+   </BrowserRouter>
   )
 }
 
