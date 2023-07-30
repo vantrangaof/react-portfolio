e01fa5c210acbf992da17b20468048b0c10749ec
trangvan <trang@akocommerce.com>
2023-07-30 16:50:55 +0700

    init nav bar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link
+        <Link // when the user clicks the logo > back to homepage, scroll to top
           to="/"
           className="flex items-center gap-2"
           onClick = {() => {
@@ -25,6 +25,22 @@ const Navbar = () => {
           <p className="text-white text-[18px] font-bold cursor-pointer">Trang
           <span className="sm:block hidden">Dev</span></p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li key={link.id}
+                className={ // check if the nav link is active
+                  `${
+                    active === link.title
+                    ? "text-white"
+                    : "text-secondary"
+                  } hover:text-white text-[18px] font-medium cursor-pointers`
+                }
+                onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
