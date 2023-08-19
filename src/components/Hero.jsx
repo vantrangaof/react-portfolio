b9000ffcb7634fc5b667dfef01d5cbc97a9e31a1
trangvan <vantrangaof.01@gmail.com>
2023-08-19 14:17:11 +0700

    install the legacy three package

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 // import {motion} from 'framer-motion'
 import {styles} from '../styles'
-// import { ComputersCanvas} from './Canvas'
+import { ComputersCanvas} from './Canvas'
 
 const Hero = () => {
   return (
@@ -8,9 +8,24 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
 
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I&apos;m
+            <span className="text-[#915eff]">&nbsp;
+               Trang
+            </span>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              I&apos;m a frontend developer, UI/UX designer <br className="sm:block hidden"/>and web applications developer
+            </p>
+          </h1>
         </div>
       </div>
+
+      <ComputersCanvas />
+
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+// import { Suspense, useEffect, useState } from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import { CanvasLoader } from '../Loader'
 
 const Computers = () => {
+
+  const computer = useGLTF('./desktop_pc/cat.gltf ')
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}
+      groundColor="black"
+      />
+      <pointLight intensity={1} />
+      <primitive object={computer.cat} />
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameLoop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25}}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
   )
 }
 
